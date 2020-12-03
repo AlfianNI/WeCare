68cb17d2a2eaf73d928304e75387ba5a02e0e096
AlfianNI <72936082+AlfianNI@users.noreply.github.com>
2020-12-03 09:38:28 +0700

    V02

---
+++ src/app/page-dokter/page-dokter.model.ts
@@ -0,0 +1,9 @@
+export interface PageDokter {
+    // Info Pribadi Dokter
+    id_dokter: string;
+    nama_dokter: string;
+    tempat_lahir: string;
+    tanggal_lahir: string;
+    nama_RS_bertugas: string;
+}
+
---
+++ src/app/pasien/pasien.model.ts
@@ -0,0 +1,9 @@
+export interface Pasien {
+    // Info Pasien
+    id_pasien: string;
+    nama_pasien: string;
+    ruangan_perawatan: string;
+    no_bpjs_pasien: string;
+    nama_RS: string;
+    nama_dokter: string;
+}
